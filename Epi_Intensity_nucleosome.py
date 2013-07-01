533f0454ebd19ede6f30466755728880031a6b80
yu68 <yupf05@gmail.com>
2013-07-01 15:42:24 -0700

    modify Epi_Intensity_nucleosome

--- Epi_Intensity_nucleosome.py
+++ Epi_Intensity_nucleosome.py
@@ -7,21 +7,27 @@ import numpy as np
 def ParseArg():
     p=argparse.ArgumentParser(description="get intensity of epi-modification on single nucleosome",epilog="library dependency: xplib")
     p.add_argument("-N","--Nucleosome",dest="nucleosome",type=str,help="xls file containing nucleosome location information from Danpos output")
-    p.add_argument("-b","--beds",nargs='+',dest="beds",type=str,help="bed files for epigenetic data")
+    p.add_argument("-b","--beds",nargs='+',dest="beds",type=str,help="bed/bam files for epigenetic data")
+    p.add_argument('-f','--fmt',type=str,default='bed',help='format: bed/bam,default:bed')
     p.add_argument("-l","--length",dest="len",type=int,default=200,help="average length of ChIP-seq fragment,default:200")
     p.add_argument("-n","--name",nargs='+',dest='name',type=str,help='name of each bed sample (to be wrote on the header)')
+    p.add_argument("-w","--weightP",nargs='+',dest='weightP',type=int,default=[75,125],help='parameters to calculate the weight for each read,[half_len of core nucleosome region and half_len of whole regions],default: [75,125]')
     p.add_argument("-o","--output",dest="output",type=str,help="output file name (can be .txt)")
     if len(sys.argv)==1:
         print >>sys.stderr,p.print_help()
         exit(0)
     return p.parse_args()
 
-def find_center(read,half_len):
+def find_center(read,half_len,fmt):
 
     # read.id in strand for bed with for columns
-    if read.id=="+":
+    if fmt=='bed':
+        strand=read.id
+    elif fmt=='bam':
+        strand=read.strand
+    if strand=="+":
         center=read.start+half_len
-    elif read.id=="-":
+    elif strand=="-":
         center=read.stop-half_len
     else:
         print >>sys.stderr,read.id,"No strand"
@@ -40,9 +46,10 @@ def find_center(read,half_len):
 
 '''
 
-global mi,ma
-mi=75
-ma=125
+global mi,ma,args
+args=ParseArg()
+mi=args.weightP[0]
+ma=args.weightP[1]
 
 
 def Main():
@@ -55,7 +62,7 @@ def Main():
     for i in range(len(args.beds)):
         temp_name=args.name[i]
         print >>sys.stderr,"  #Indexing for bed file of",temp_name,"\r",
-        bed[temp_name]=DBI.init(args.beds[i],'bed')
+        bed[temp_name]=DBI.init(args.beds[i],args.fmt)
 
     half_len=int(args.len)
     print >>sys.stderr
@@ -79,9 +86,13 @@ def Main():
         count=np.zeros(len(args.beds),dtype="float")
         line=str(i)
         for k,name in enumerate(bed.keys()):
-            for j in bed[name].query(Bed([chrom,center-ma-(half_len-75),center+ma+(half_len-75)])):
-                j_center=find_center(j,half_len)
-                weight = max(min(1,(ma-abs(j_center-center))/25.0),0)
+            if args.fmt=='bam':
+                query=bed[name].query(Bed([chrom,center-ma-(half_len-75),center+ma+(half_len-75)]),method='fetch')
+            else:
+                query=bed[name].query(Bed([chrom,center-ma-(half_len-75),center+ma+(half_len-75)]))
+            for j in query:
+                j_center=find_center(j,half_len,args.fmt)
+                weight = max(min(1,(ma-abs(j_center-center))*1.0/(ma-mi)),0)
                 count[k]+=weight
         line = line + "\t" + "\t".join(str(f) for f in count)
         print >>out,line
